67edbd9a3df386021311fb9eee807d6a1fa55518
Rohit <rohitmondall8000@gmail.com>
2023-11-06 08:30:30 +0530

    initial commi

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import './App.css'
+
+function App() {
+  return (
+    <section>
+      <div className='card card1'>
+        <h1>Card 1</h1>
+      </div>
+
+      <div className='card card2'>
+        <h1>Card 2</h1>
+      </div>
+
+      <div className='card card3'>
+        <h1>Card 3</h1>
+      </div>
+
+      <div className='card card4'>
+        <h1>Card 4</h1>
+      </div>
+    </section>
+  )
+}
+
+export default App
